f32a26955590d3ec4d4b72eb9e54569d0c12b099
FSDonnelly <donnelly_frank@yahoo.com>
2019-07-04 20:02:03 -0700

    add Form and FormStyles

---
+++ src/styles/FormStyles.js
@@ -0,0 +1,14 @@
+const styles = theme => ({
+    main: {
+        width: "auto",
+        display: "block",
+        marginLeft: theme.spacing(3),
+        marginRight: theme.spacing(3),
+        [theme.breakpoints.up("md")]: {
+            width: 400,
+            marginLeft: "autp",
+            marginRight: "auto"
+        }
+    }
+});
+export default styles;
