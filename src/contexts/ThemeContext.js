e78304b586aa7b58b409e9509f52b4803eeac109
FSDonnelly <donnelly_frank@yahoo.com>
2019-07-06 00:11:34 -0700

    updating context dynamically

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,10 +6,13 @@ export class ThemeProvider extends Component {
     constructor(props) {
         super(props);
         this.state = { isDarkMode: false }
-    }
+    };
+    toggleTheme = () => {
+        this.setState({ isDarkMode: !this.state.isDarkMode });
+    };
     render() {
         return (
-            <ThemeContext.Provider value={{ ...this.state, tastesLikeChicken: true }}>
+            <ThemeContext.Provider value={{ ...this.state, toggleTheme: this.toggleTheme }}>
                 {this.props.children}
             </ThemeContext.Provider>
         )
